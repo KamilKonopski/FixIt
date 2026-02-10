6d7fbcdd657c616ba8a5f2f8d934f55e205f6778
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2026-02-10 17:06:36 +0100

    feat: implement filters params

--- FixIt-frontend/src/store/tickets/ticketsApi.types.ts
+++ FixIt-frontend/src/store/tickets/ticketsApi.types.ts
@@ -5,6 +5,16 @@ export type TicketStatus =
   | "Resolved"
   | "Closed";
 
+export type SortDirection = "Asc" | "Desc";
+
+export type GetTicketsParams = {
+  page?: number;
+  pageSize?: number;
+  search?: string;
+  status?: TicketStatus;
+  sort?: SortDirection;
+};
+
 export interface TicketResponse {
   id: string;
   title: string;
@@ -18,4 +28,11 @@ export interface TicketResponse {
 export interface TicketRequest {
   title: string;
   description: string;
+}
+
+export interface PaginatedTicketsResponse {
+  totalCount: number;
+  page: number;
+  pageSize: number;
+  result: TicketResponse[];
 }
