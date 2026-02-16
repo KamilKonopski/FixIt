a2c60d819a3799456d0f55f26ce2d8d831dd2bb9
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2026-02-16 14:40:09 +0100

    add note and history entities

---
+++ FixIt-backend/src/FixIt.Domain/Entities/TicketNote.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FixIt.Domain.Entities
+{
+    public class TicketNote
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid TicketId { get; set; }
+        public Guid AuthorId { get; set; }
+
+        [ForeignKey("TicketId")]
+        public virtual Ticket Ticket { get; set; } = null!;
+
+        [ForeignKey("AuthorId")]
+        public virtual User Author { get; set; } = null!;
+    }
+}
